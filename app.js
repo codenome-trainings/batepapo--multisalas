533c13e9fa416bffa1a3ebac74814995401f3512
Thiago Cunha <grupocodenome@gmail.com>
2018-07-24 23:16:40 -0300

    Configuração socket.io para multcast ao vivo

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var app = require('./config/server');
 
-app.listen(80, function() {
+var server = app.listen(80, function() {
     console.log('Servidor online na porta 80');
 })
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket) {
+    console.log('Usuario conectou: ');
+
+    socket.on('disconnect', function() {
+        console.log('Usuario desconectou');
+    })
+});
